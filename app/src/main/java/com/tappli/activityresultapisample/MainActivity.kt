bb5fadf01e45f3d028b9f2711248b7d24d0539b9
Tetsuro Nakamura <t2low.nk@gmail.com>
2020-09-24 22:50:31 +0900

    GetContentを試す

--- app/src/main/java/com/tappli/activityresultapisample/MainActivity.kt
+++ app/src/main/java/com/tappli/activityresultapisample/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.tappli.activityresultapisample
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+        Toast.makeText(this, uri?.toString() ?: "cancelled", Toast.LENGTH_SHORT).show()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        selectButton.setOnClickListener {
+            getContent.launch("image/*")
+        }
     }
 }
